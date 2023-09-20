8e59d33f595eb047fc6401b1f724ace775fb69b5
Kara <10913049@uvu.edu>
2023-09-20 16:26:14 -0600

    midworksave3

--- unit_1a/unit_1a-Challenge.cs
+++ unit_1a/unit_1a-Challenge.cs
@@ -12,7 +12,8 @@ namespace MyApplication
       Console.WriteLine("Name is: " + userName);
       Console.WriteLine("Thank you, now please give us your age.");
       string dateOfBirth = Console.ReadLine();
-      Console.WriteLine("Age is: " + dateOfBirth);
+      Console.WriteLine("Age is: " + dateOfBirth); 
+      //I started off with getting the name and birth
       int time = 13;
       if (time < 8 + 2)
       {
@@ -25,10 +26,28 @@ namespace MyApplication
       else 
       {
         Console.WriteLine("Good evening." + userName);
-	  } 
-      Console.WriteLine("now that we are ready to begin, please input your favorite liveaction Spider-man!");
-      Console.WriteLine("either, Tobey_Maguire, Andrew_Garfield or Tom_Holland");
-      int spider = Convert.ToInt32(Console.ReadLine());
+      //here ive included a little welcome with there name, which changes depending on the time of day.
+      Console.WriteLine("Now that we are ready to begin, please input your favorite live-action Spider-man!");
+      Console.WriteLine("you may either enter, Tobey_Maguire, Andrew_Garfield or Tom_Holland");
+      int spider = Convert.ToInt32(Console.ReadLine()); //int is very important here
+      if (spider = Tobey_Maguire)
+      {
+      Console.WriteLine("Tobey is an awesome choice! Thank you for completing the Survey!");
+      } 
+      else if (spider = Andrew_Garfield)
+      {
+      Console.WriteLine("Andrew is an awesome choice! Thank you for completing the Survey!");
+      }
+      else if (spider = Tom_Holland)
+      {
+      Console.WriteLine("Tom is an awesome choice! Thank you for completing the Survey!");
+      }
+      else 
+      {
+      Console.WriteLine("Please re-input your response by copy and pasting one of the three actors names above.");  
+      }
+      //lastly, i took the input of there favorite spiderman and gave them responses depending on the answer. 
+      //if one of the responses doesn't work, i ask them to resubmit by copy and paste.
     }
   }
 }
