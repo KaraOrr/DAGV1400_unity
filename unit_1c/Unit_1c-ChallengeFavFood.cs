88c206c79f018d59d9f5c6d05ed7523b8a6dab36
kara <karaorr4@gmail.com>
2023-09-26 18:04:16 -0600

    challenge_1c_assignment

--- unit_1c/Unit_1c-ChallengeFavFood.cs
+++ unit_1c/Unit_1c-ChallengeFavFood.cs
@@ -1,12 +1,15 @@
 using System;
-
-public class Program
-{
-	public static void Main()
-	{
-    Console.WriteLine("please enter your favorite food, one at a time!") 
-    guess = Convert.ToInt32(Console.ReadLine());
-    guess = Convert.ToInt32(Console.ReadLine());
-    guess = Convert.ToInt32(Console.ReadLine());
-	}
+class HelloWorld {
+  static void Main() {
+    Console.WriteLine("Please enter your top 3 favorite foods, one at a time!");
+    string foodA = Convert.ToString(Console.ReadLine()); //input 1
+    string foodB = Convert.ToString(Console.ReadLine()); //input 2
+    string foodC = Convert.ToString(Console.ReadLine()); //input 3
+    string[] foods = {foodA, foodB, foodC}; //putting them in array
+        for (int i = 0; i < foods.Length; i++) 
+        {
+        Console.WriteLine(foods[i] + " is so delicous."); //printing out array
+        }
+    Console.WriteLine("Nice! " + foods[0] +", "+ foods[1] +", and "+ foods[2] + " are some great foods!" ); //cherry on top!
+  }
 }
